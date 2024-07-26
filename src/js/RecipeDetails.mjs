78221dae95b14ebaad747b43b0863774e34bdfcc
Hatsumi07 <crystalhatsumi07@gmail.com>
2024-07-25 21:32:59 -0500

    added coments to recipe details page

--- src/js/RecipeDetails.mjs
+++ src/js/RecipeDetails.mjs
@@ -1,5 +1,12 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, renderListWithTemplate } from "./utils.mjs";
 
+function convertToJson(res) {
+  if (res.ok) {
+    return res.json();
+  } else {
+    throw new Error("Bad Response");
+  }
+}
 function renderIngredients(list) {
   const htmlList = list.map(item => `<li>${item.raw_text}</li>`);
   return htmlList.join(" ");
@@ -20,7 +27,14 @@ function renderNutritionFacts(object) {
   }
   return htmlList.join("");
 }
-
+function recipeCommentsTemplate(comment) {
+  return `
+  <li class="recipe-comment">
+    <img src="/images/user-icon.png">
+    <h3>${comment.author_username}</h3>
+    <p>${comment.tip_body}</p>
+  </li>`;
+}
 function recipeDetailsTemplate(recipe) {
     return `<h1 class="name">${recipe.name}</h1>
         <img class="main-img" src=${recipe.thumbnail_url}>
@@ -56,7 +70,6 @@ function recipeDetailsTemplate(recipe) {
   export default class ProductDetails {
     constructor(id, dataSource) {
       this.recipeId = id;
-      // this.category = category;
       this.recipe = {};
       this.dataSource = dataSource;
     }
@@ -64,15 +77,9 @@ function recipeDetailsTemplate(recipe) {
       const recipes = await this.dataSource.getData();
       const recipeById = recipes.filter(recipe => recipe.id == this.recipeId);
       this.recipe = recipeById[0];
-      console.log("recipe: ", recipes);
       
       this.renderRecipeDetails();
-      // once the HTML is rendered we can add a listener to Add to Cart button
-      // Notice the .bind(this). Our callback will not work if we don't include that line. Review the readings from this week on 'this' to understand why.
-      
-      // document
-      //   .getElementById("addToCart")
-      //   .addEventListener("click", this.addProductToCart.bind(this));
+      await this.displayComments(this.recipeId);
     }
     addProductToCart() {
         let cartItems = getLocalStorage("so-cart") || [];
@@ -86,5 +93,24 @@ function recipeDetailsTemplate(recipe) {
         "afterBegin",
         recipeDetailsTemplate(this.recipe)
       );
+    }
+    async displayComments(id) {
+      const element = document.querySelector("#recipe-comments");
+      const url = `https://tasty.p.rapidapi.com/tips/list?from=0&size=3&id=${id}`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': '1aff26895cmsh06f0d822468b8efp1377bdjsn38be06299bd2',
+          'x-rapidapi-host': 'tasty.p.rapidapi.com'
+        }
+      };
+  
+      try {
+        const response = await fetch(url, options);
+        const data = await convertToJson(response);
+        renderListWithTemplate(recipeCommentsTemplate, element, data.results);
+      } catch (error) {
+        console.error(error);
+      }
     }
   }
